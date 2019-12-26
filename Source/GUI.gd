78f37a87d086d29d5cf48e9adb819b778a6957f7
ihavebeenjamin <42360305+ihavebeenjamin@users.noreply.github.com>
2019-12-26 16:06:36 -0600

    Harvesting
    -Contains pickaxe
    - Throws everything into main scene
    - Has a beautiful GUI
    - Can harvest rocks
    - Collision with environment and other player in game

---
+++ Source/GUI.gd
@@ -0,0 +1,16 @@
+extends Node
+#RESOURCE_MANAGER
+var stone_count = 0
+onready var stone_label = $HBoxContainer/Counters/Counter/Background/Number
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	stone_label.text = str(stone_count)
+	pass # Replace with function body.
+
+func _add_stone():
+	stone_count +=1
+	stone_label.text = str(stone_count)
+#func _process(delta):
+#	pass
--- Source/Ship.gd
+++ Source/Ship.gd
@@ -1,7 +1,10 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 var speed = 150
 var Mouse_position
+
+#------Harvesting vars--------
+var harvesting  = false
 
 #-----Equipment vars-----
 var is_equipped = false
@@ -10,17 +13,18 @@ var current_equipID = 0
 
 slave var slave_position = Vector2()
 slave var slave_rotation = 0
+var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _get_input():
 	Mouse_position=get_local_mouse_position()
-	var velocity = Vector2()  # The player's movement vector.
+	
 	if is_network_master():
-		
+		velocity = Vector2()
 		#-----Movement Block-----
 		if Input.is_action_pressed("move_right"):
 			velocity.x += 1
@@ -32,7 +36,7 @@ func _process(delta):
 			velocity.y -= 1
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
-		position += velocity * delta
+		#position += velocity * delta
 		rotation += Mouse_position.angle()
 #		position.x = clamp(position.x, 0, screen_size.x)
 #		position.y = clamp(position.y, 0, screen_size.y)
@@ -52,14 +56,18 @@ func _process(delta):
 			get_node("AnimationPlayer").play("Spear_Attack")
 		
 		if Input.is_action_just_pressed("left_click"):
+			harvesting = true;
 			get_node("AnimationPlayer").play("Pickaxe_tex")
-		
+			harvesting = false;
 		
 		
 	else:
 		position = slave_position
 		rotation = slave_rotation
-
+#Benjamins ADDED CODE to try and make collisions work
+func _physics_process(delta):
+	_get_input()
+	move_and_collide(velocity * delta)
 
 #This function will equip a set item:
 #---Purpose---
@@ -71,14 +79,13 @@ func equip(id):
 			$Pickaxe_tex.show()
 			current_equipID=1
 	pass
-
-
+	
 func unequip(id):
 	match id:
 		1: #ID 1 = Spear
 			$Pickaxe_tex.hide()
 			current_equipID = 0
 	is_equipped = false
-	
 
+	
 
